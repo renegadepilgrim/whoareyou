c6fb277ee7ddc2612f7ac02d0ab5af131191f8e2
Heather Knight and Tyler Ferrin <tyler@>
2016-09-21 20:46:36 -0700

    completes our functioning which friend are you website

--- js/scriptz.js
+++ js/scriptz.js
@@ -1,29 +1,36 @@
 $(document).ready(function() {
-  var answer1 = parseInt($("#fear").val());
-  var answer2 = parseInt($("#values").val());
-  var answer3 = parseInt($("#coffee").val());
-  var userScore = answer1 + answer2 + answer3;
-
   $("form#questions").submit(function(event) {
     event.preventDefault();
-    
-    if ( userScore === 3 ) {
+
+    var answer1 = parseInt($("#fear").val());
+    var answer2 = parseInt($("#values").val());
+    var answer3 = parseInt($("#coffee").val());
+    var userScore = answer1 + answer2 + answer3;
+
+
+    if (userScore === 3) {
       $("#result").addClass("ross");
+      $("form#questions").hide();
       $("#result").show();
-    } else if ( userScore >= 4 && <= 6 ) {
+    } else if (userScore >= 4 && userScore <= 6) {
       $("#result").addClass("joey");
+      $("form#questions").hide();
       $("#result").show();
-    } else if ( userScore >= 7 && <= 9 ) {
+    } else if ( userScore >= 7 && userScore <= 9 ) {
       $("#result").addClass("rachel");
+      $("form#questions").hide();
       $("#result").show();
-    } else if ( userScore >= 10 && <= 12 ) {
+    } else if ( userScore >= 10 && userScore<= 12 ) {
       $("#result").addClass("monica");
+      $("form#questions").hide();
       $("#result").show();
-    } else if ( userScore >= 13 && <= 15 ) {
+    } else if ( userScore >= 13 && userScore<= 15 ) {
       $("#result").addClass("chandler");
+      $("form#questions").hide();
       $("#result").show();
-    } else if ( userScore >= 16 && <= 18 ) {
+    } else if ( userScore >= 16 && userScore<= 18 ) {
       $("#result").addClass("pheebs");
+      $("form#questions").hide();
       $("#result").show();
     }
   });
